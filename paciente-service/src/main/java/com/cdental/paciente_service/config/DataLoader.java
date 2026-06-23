05f2831a256d5bfd76b34b803cb92166e64112cf
mauretam <ma.uretam@duocuc.cl>
2026-06-23 03:26:50 -0400

    fix: unificar formato extendido en DataLoader para mantener consistencia

--- paciente-service/src/main/java/com/cdental/paciente_service/config/DataLoader.java
+++ paciente-service/src/main/java/com/cdental/paciente_service/config/DataLoader.java
@@ -16,12 +16,22 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         if (repository.count() <= 7) {
-            Paciente p1 = new Paciente(); p1.setRut("88.888.888-8"); p1.setNombre("Diego Diaz"); p1.setCorreo("diego@mail.com");
-            Paciente p2 = new Paciente(); p2.setRut("99.999.999-9"); p2.setNombre("Elena Silva"); p2.setCorreo("elena@mail.com");
-            Paciente p3 = new Paciente(); p3.setRut("10.000.000-0"); p3.setNombre("Raul Muñoz"); p3.setCorreo("raul@mail.com");
-            
+            Paciente p1 = new Paciente(); 
+            p1.setRut("88.888.888-8"); 
+            p1.setNombre("Diego Diaz"); 
+            p1.setCorreo("diego@mail.com");
             repository.save(p1);
+
+            Paciente p2 = new Paciente(); 
+            p2.setRut("99.999.999-9"); 
+            p2.setNombre("Elena Silva"); 
+            p2.setCorreo("elena@mail.com");
             repository.save(p2);
+
+            Paciente p3 = new Paciente(); 
+            p3.setRut("10.000.000-0"); 
+            p3.setNombre("Raul Muñoz"); 
+            p3.setCorreo("raul@mail.com");
             repository.save(p3);
         }
     }
