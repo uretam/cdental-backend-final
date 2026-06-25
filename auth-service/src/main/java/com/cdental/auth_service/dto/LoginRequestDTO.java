67e8837754c41c291401ec9180c5bfb0f8a0988e
mauretam <ma.uretam@duocuc.cl>
2026-06-24 21:58:31 -0400

    Refactor: Completar ajustes de la rubrica

--- auth-service/src/main/java/com/cdental/auth_service/dto/LoginRequestDTO.java
+++ auth-service/src/main/java/com/cdental/auth_service/dto/LoginRequestDTO.java
@@ -1,13 +1,16 @@
 package com.cdental.auth_service.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class LoginRequestDTO {
-    @NotBlank(message = "El nombre de usuario no puede estar vacio")
+@NotBlank(message = "El nombre de usuario no puede estar vacio")
+    @Size(min = 4, max = 20, message = "El username debe tener entre 4 y 20 caracteres")
     private String username;
 
     @NotBlank(message = "La contrasena no puede estar vacia")
+    @Size(min = 6, message = "La contrasena debe tener al menos 6 caracteres")
     private String password;
 }
--- auth-service/src/main/java/com/cdental/auth_service/dto/UsuarioDTO.java
+++ auth-service/src/main/java/com/cdental/auth_service/dto/UsuarioDTO.java
@@ -1,6 +1,9 @@
 package com.cdental.auth_service.dto;
 
 import org.springframework.hateoas.RepresentationModel;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.AllArgsConstructor;
@@ -11,7 +14,13 @@ import lombok.EqualsAndHashCode;
 @AllArgsConstructor
 @EqualsAndHashCode(callSuper = false) // Evita conflictos con los enlaces de RepresentationModel de HATEOAS
 public class UsuarioDTO extends RepresentationModel<UsuarioDTO> {
-    private Long id;
+    @NotNull(message = "El id no puede ser nulo")
+    private Long id; 
+
+    @NotBlank(message = "El nombre de usuario no puede estar vacio")
+    @Size(min = 4, max = 20, message = "El username debe tener entre 4 y 20 caracteres")
     private String username;
+
+    @NotBlank(message = "El rol no puede estar vacio")
     private String role;
 }
